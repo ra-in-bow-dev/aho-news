b32ab75352ec6db1592a36d702d4678bc796985a
Untone <anton.rewin@gmail.com>
2020-12-13 17:18:02 +0300

    optimized rollup bundling and migrated to tailwind

---
+++ src/store/network.ts
@@ -0,0 +1,52 @@
+import { derived, Readable, Writable, writable } from 'svelte/store'
+import type {
+  ConnectedPeer,
+  SBClientOptions,
+  Switchboard as SBType,
+} from 'switchboard.js'
+import { Switchboard } from 'switchboard.js'
+
+export interface Message {
+  id?: string // FIXME: who is generating message id?
+  type: string // can be: image audio video text fix aho rate
+  body: string // code to inject
+  rate?: number // points of respect
+  reply_to?: string // other message id
+  timestamp?: number // on message dynamic stamp
+  from?: string // author peer id
+}
+
+export const options: SBClientOptions = {
+  trackers: [
+    'wss://tracker.sloppyta.co:443/announce',
+    'wss://tracker.files.fm:7073/announce',
+    'wss://tracker.openwebtorrent.com',
+  ],
+}
+
+export const connection: Writable<SBType> = writable(new Switchboard(options))
+export const peers: Writable<Map<string, ConnectedPeer>> = writable(new Map())
+export const seens: Writable<Map<string, number>> = writable(new Map())
+export const messages: Writable<Array<Message>> = writable([])
+export const threads: Readable<Array<Message>> = derived(
+  [messages],
+  ([$messages]) =>
+    $messages
+      .filter((m) => !m.reply_to)
+      .sort((a, b) => (a.timestamp > b.timestamp ? 1 : -1))
+)
+
+export const network = {
+
+    send: (msg) => {
+        // msg.reply_to === null is a new thread
+        // 
+    },
+
+    disconnect: () => {
+        // breaks connection
+        // keeps message log
+    },
+
+    
+}
